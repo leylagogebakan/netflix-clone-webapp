2e20fa88d5e097f2ff3446cd894f49515da99f6c
Saket Kothari <saketkothari722@gmail.com>
2022-10-04 09:07:17 +0530

    Creating sticky navbar

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,10 +1,12 @@
 import Navbar from '../components/Navbar/Navbar';
 import './home.scss';
+import profile from '../assets/profile.webp';
 
 const Home = () => {
   return (
     <div className="home">
       <Navbar />
+      <img width="100%" src={profile} alt="Profile" />
     </div>
   );
 };
