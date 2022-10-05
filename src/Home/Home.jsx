f55a74fc4d2b102cec0d7e15a8131f740b748dea
Saket Kothari <saketkothari722@gmail.com>
2022-10-05 09:13:59 +0530

    Netflix Featured Movie Section

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,12 +1,12 @@
 import Navbar from '../components/Navbar/Navbar';
 import './home.scss';
-import profile from '../assets/profile.webp';
+import Featured from '../components/Featured/Featured';
 
 const Home = () => {
   return (
     <div className="home">
       <Navbar />
-      <img width="100%" src={profile} alt="Profile" />
+      <Featured type="series" />
     </div>
   );
 };
