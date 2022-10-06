fdcdd65db96ffd346f6b3ff79eaff931ca0a8fd5
Saket Kothari <saketkothari722@gmail.com>
2022-10-06 22:40:55 +0530

    Adding image to the card

--- src/components/ListItem/ListItem.jsx
+++ src/components/ListItem/ListItem.jsx
@@ -1,7 +1,24 @@
 import './listItem.scss';
+import Card from '../../assets/card.jpg';
+import {
+  Add,
+  PlayArrow,
+  ThumbDownAltOutlined,
+  ThumbDownOutlined,
+} from '@material-ui/icons';
 
 export default function ListItem() {
   return (
-    <div className='listItem'>ListItem</div>
-  )
+    <div className="listItem">
+      <img src={Card} alt="card" />
+      <div className="itemInfo">
+        <div className="icons">
+          <PlayArrow />
+          <Add />
+          <ThumbDownAltOutlined />
+          <ThumbDownOutlined />
+        </div>
+      </div>
+    </div>
+  );
 }
