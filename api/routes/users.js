669b1b4ad00106161bb2fa81e00c005c9a0e0693
Saket Kothari <saketkothari722@gmail.com>
2022-12-25 18:03:28 +0530

    Get a single user, Get all user and delete a user

--- api/routes/users.js
+++ api/routes/users.js
@@ -34,10 +34,47 @@ router.put('/:id', verify, async (req, res) => {
 });
 
 // Delete
+router.delete('/:id', verify, async (req, res) => {
+  if (req.user.id === req.params.id || req.user.isAdmin) {
+    try {
+      await User.findByIdAndDelete(req.params.id);
+      res.status(200).json('User has been deleted successfully!');
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  } else {
+    res.status(403).json('You can delete only your account!');
+  }
+});
 
 // Get
+router.get('/find/:id', async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    // we don't want to see password
+    const { password, ...info } = user._doc;
+    res.status(200).json(info);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
 
 // Get all
+router.get('/', verify, async (req, res) => {
+  const query = req.query.new;
+  if (req.user.isAdmin) {
+    try {
+      const users = query
+        ? await User.find().sort({ _id: -1 }).limit(10)
+        : await User.find();
+      res.status(200).json(users);
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  } else {
+    res.status(403).json('You are not allowed to see all users!');
+  }
+});
 
 // Get user stats
 
