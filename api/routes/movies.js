29beac90e6be5f9ab3595f430053e8d866b0a39d
Saket Kothari <saketkothari722@gmail.com>
2022-12-27 22:42:38 +0530

    Get random & all movies route

--- api/routes/movies.js
+++ api/routes/movies.js
@@ -53,12 +53,52 @@ router.delete('/:id', verify, async (req, res) => {
 });
 
 // Get a movie
-router.get('/:id', verify, async (req, res) => {
+router.get('/find/:id', verify, async (req, res) => {
   try {
     const movie = await Movie.findById(req.params.id);
     res.status(200).json(movie);
   } catch (error) {
     res.status(500).json(error);
+  }
+});
+
+// Get random movie
+router.get('/random', verify, async (req, res) => {
+  const type = req.query.type;
+  let movie;
+  try {
+    if (type === 'series') {
+      movie = await Movie.aggregate([
+        // find all series
+        { $match: { isSeries: true } },
+        // give one random
+        { $sample: { size: 1 } },
+      ]);
+    } else {
+      movie = await Movie.aggregate([
+        // find all movies
+        { $match: { isSeries: false } },
+        // give one random
+        { $sample: { size: 1 } },
+      ]);
+    }
+    res.status(200).json(movie);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+// Get all movies
+router.get('/', verify, async (req, res) => {
+  if (req.user.isAdmin) {
+    try {
+      const movies = await Movie.find();
+      res.status(200).json(movies.reverse());
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  } else {
+    res.status(403).json('You are not allowed!');
   }
 });
 
