183586414bf32173270290c468791f21302d1325
Saket Kothari <saketkothari722@gmail.com>
2022-12-29 21:19:02 +0530

    Using react router dom

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,34 @@
 import './app.scss';
-import Login from './pages/Login/Login';
+// import Login from './pages/Login/Login';
 // import Register from './pages/Register/Register';
-// import Home from './pages/Home/Home';
-// import Watch from './pages/Watch/Watch';
+import Home from './pages/Home/Home';
+import Watch from './pages/Watch/Watch';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom';
 
 const App = () => {
-  return <Login />;
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/movies">
+          <Home type="movies" />
+        </Route>
+        <Route path="/series">
+          <Home type="series" />
+        </Route>
+        <Route path="/watch">
+          <Watch />
+        </Route>
+      </Switch>
+    </Router>
+  );
 };
 
 export default App;
--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -3,11 +3,11 @@ import './home.scss';
 import Featured from '../../components/Featured/Featured';
 import List from '../../components/List/List';
 
-const Home = () => {
+const Home = ({ type }) => {
   return (
     <div className="home">
       <Navbar />
-      <Featured type="series" />
+      <Featured type={type} />
       <List />
       <List />
       <List />
