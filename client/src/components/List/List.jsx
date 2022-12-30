0f4cb0454de929c3c6af5ed5354b2c255ef50824
Saket Kothari <saketkothari722@gmail.com>
2022-12-30 22:14:13 +0530

    React Fetch Data from API and showing lists

--- client/src/components/List/List.jsx
+++ client/src/components/List/List.jsx
@@ -6,7 +6,7 @@ import { useRef, useState } from 'react';
 import ListItem from '../ListItem/ListItem';
 import './list.scss';
 
-export default function List() {
+export default function List({ list }) {
   const [isMoved, setIsMoved] = useState(false);
   const [slideNumber, setSlideNumber] = useState(0);
   const listRef = useRef();
@@ -28,7 +28,7 @@ export default function List() {
 
   return (
     <div className="list">
-      <span className="listTitle">Continue to watch</span>
+      <span className="listTitle">{list.title}</span>
       <div className="wrapper">
         <ArrowBackIosOutlined
           className="sliderArrow left"
@@ -36,16 +36,9 @@ export default function List() {
           style={{ display: !isMoved && 'none' }}
         />
         <div className="container" ref={listRef}>
-          <ListItem index={0} />
-          <ListItem index={1} />
-          <ListItem index={2} />
-          <ListItem index={3} />
-          <ListItem index={4} />
-          <ListItem index={5} />
-          <ListItem index={6} />
-          <ListItem index={7} />
-          <ListItem index={8} />
-          <ListItem index={9} />
+          {list.content.map((item, i) => (
+            <ListItem key={i} index={i} item={item} />
+          ))}
         </div>
         <ArrowForwardIosOutlined
           className="sliderArrow right"
--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -2,16 +2,45 @@ import Navbar from '../../components/Navbar/Navbar';
 import './home.scss';
 import Featured from '../../components/Featured/Featured';
 import List from '../../components/List/List';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 const Home = ({ type }) => {
+  const [lists, setLists] = useState([]);
+  const [genre, setGenre] = useState(null);
+
+  // Whenever we change type & genre it's gonna call this useEffect
+  useEffect(() => {
+    const getRandomLists = async () => {
+      try {
+        // url is list with type either series or movies and if we have genre as query
+        const res = await axios.get(
+          `lists${type ? '?type=' + type : ''}${
+            genre ? '&genre=' + genre : ''
+          }`,
+          {
+            headers: {
+              token:
+                'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzYWIyMzM2OThmMzg4NmJlOGMzMmU1ZSIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY3MjQxNjcxNSwiZXhwIjoxNjcyODQ4NzE1fQ.fDEXYMQPkx_6FkRIMyOkIE5Mb5-Y_XH4SSQsnirg9jw',
+            },
+          }
+        );
+        // console.log(res);
+        setLists(res.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getRandomLists();
+  }, [type, genre]);
+
   return (
     <div className="home">
       <Navbar />
       <Featured type={type} />
-      <List />
-      <List />
-      <List />
-      <List />
+      {lists.map((list, i) => (
+        <List key={i} list={list} />
+      ))}
     </div>
   );
 };
