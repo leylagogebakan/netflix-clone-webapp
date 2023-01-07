1e280946a62d978da6a9fe381d2bae5392b15425
Saket Kothari <saketkothari722@gmail.com>
2023-01-07 10:49:21 +0530

    Handling POST request on login (login page)

--- admin/src/pages/login/Login.jsx
+++ admin/src/pages/login/Login.jsx
@@ -1,13 +1,40 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
+import { login } from '../../context/authContext/apiCalls';
+import { AuthContext } from '../../context/authContext/AuthContext';
 import './login.css';
 
 export default function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const { isFetching, dispatch } = useContext(AuthContext);
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    login({ email, password }, dispatch);
+  };
+
   return (
     <div className="login">
       <form className="loginForm">
-        <input type="text" placeholder="Email" className="loginInput" />
-        <input type="password" placeholder="Password" className="loginInput" />
-        <button className="loginButton">Login</button>
+        <input
+          onChange={(e) => setEmail(e.target.value)}
+          type="text"
+          placeholder="Email"
+          className="loginInput"
+        />
+        <input
+          onChange={(e) => setPassword(e.target.value)}
+          type="password"
+          placeholder="Password"
+          className="loginInput"
+        />
+        <button
+          onClick={handleLogin}
+          className="loginButton"
+          disabled={isFetching}
+        >
+          Login
+        </button>
       </form>
     </div>
   );
