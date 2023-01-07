e1dff6ff0c0221b52de2fdf490f2755eacce2ccd
Saket Kothari <saketkothari722@gmail.com>
2023-01-07 19:02:21 +0530

    Saving the user in local storage - React login

--- admin/src/context/authContext/AuthContext.js
+++ admin/src/context/authContext/AuthContext.js
@@ -1,8 +1,9 @@
 import AuthReducer from './AuthReducer';
 import { createContext, useReducer } from 'react';
+import { useEffect } from 'react';
 
 const INITIAL_STATE = {
-  user: null,
+  user: JSON.parse(localStorage.getItem('user')) || null,
   isFetching: false,
   error: false,
 };
@@ -12,6 +13,10 @@ export const AuthContext = createContext(INITIAL_STATE);
 export const AuthContextProvider = ({ children }) => {
   // dispatch our actions
   const [state, dispatch] = useReducer(AuthReducer, INITIAL_STATE);
+
+  useEffect(() => {
+    localStorage.setItem('user', JSON.stringify(state.user));
+  }, [state.user]);
 
   return (
     <AuthContext.Provider
